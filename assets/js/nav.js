ca44a3081d731b2851af332ca4b983fac1f0be26
Maciel p <mponce.escosa@gmail.com>
2022-08-29 21:25:13 -0500

    comienzo

---
+++ assets/js/nav.js
@@ -0,0 +1,9 @@
+function openNav(){
+    document.getElementById("mobile-menu").style.width = "100%";
+}
+
+function closeNav(){
+    document.getElementById("mobile-menu").style.width = "0%";
+
+
+
