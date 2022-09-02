88d2e22aab85da1ecba672a7d6b4c5bec100d177
Maciel p <mponce.escosa@gmail.com>
2022-09-02 00:57:17 -0500

    nueva

--- assets/js/appTienda.js
+++
@@ -1,5 +0,0 @@
-var productos = [
-    { id: 1, nombre: 'Serum Anti-acné', precio: 499.00},
-    { id: 2, nombre: 'Hidratante', precio: 549.00},
-
-]
--- assets/js/navBar.js
+++
@@ -1,7 +0,0 @@
-function openNav(){
-    document.getElementById("mobile-menu").style.width = "100%";
-}
-
-function closeNav(){
-    document.getElementById("mobile-menu").style.width = "0%";
-}
