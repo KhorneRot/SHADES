3f249a92aea769683f15bb943ae7c5b964297957
Josselyn <jossm@ciencias.unam.mx>
2022-09-21 18:09:45 -0500

    Tienda y producto

--- assets/js/producto.js
+++ assets/js/producto.js
@@ -8,11 +8,13 @@ btns.forEach(element => {
         cont++; 
         item.cantidad = cont; 
         item.id = 5 + btns.indexOf(element); 
-        item.precio = tarjetaProd[btns.indexOf(element)].querySelector("p").textContent.slice(1); 
-        item.title = tarjetaProd[btns.indexOf(element)].querySelector("h5").textContent 
+        item.precio = tarjetaProd[btns.indexOf(element)].querySelector("h5").textContent.slice(1); 
+        item.title = tarjetaProd[btns.indexOf(element)].querySelector("p").textContent 
         let tmp = Object.values(JSON.parse(localStorage.getItem('carrito'))); 
         tmp.push(item); 
         localStorage.setItem('carrito', JSON.stringify(tmp)); 
     });
     
 });
+
+
